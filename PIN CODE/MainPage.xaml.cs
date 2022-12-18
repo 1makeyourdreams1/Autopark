9202fb0a91c4b67baf969be091fa7f741e384e7c
1makeyourdreams1 <116307262+1makeyourdreams1@users.noreply.github.com>
2022-12-18 16:28:50 +0300

    Add files via upload

---
+++ PIN CODE/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace ДЗ5;
+
+public partial class MainPage : ContentPage
+{
+	private string _password = "123";
+	private char _action = 'V';
+    public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void DigitClicked(object sender, EventArgs e)
+    {
+        if (DisplayLabel.Text != "Correct Password")
+        {
+            DisplayLabel.Text += (sender as Button).Text;
+        }
+       
+    }
+
+	private void InputClicked(object sender, EventArgs e)
+	{
+        if (DisplayLabel.Text != "Correct Password")
+        {
+            _action = Convert.ToChar((sender as Button).Text);
+            if (_action == 'V' && DisplayLabel.Text == _password)
+            {
+                DisplayLabel.Text = "Correct Password";
+
+            }
+            else
+            {
+                DisplayLabel.Text = "";
+            }
+        }
+        
+	}
+}
+
